bd4f88b165912124a1ea752641f372b140b633cf
Liliia Kuzyeva <gilmirahman@gmail.com>
2019-06-09 18:58:44 +0500

    Добавлены test_add_contact.py и contact.py

---
+++ contact.py
@@ -0,0 +1,15 @@
+__author__ = 'Liliia'
+
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, mobile, email, address2):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.mobile = mobile
+        self.email = email
+        self.address2 = address2
