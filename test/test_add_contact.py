c073bcdca59d46b721493588e4bc60684d712b8e
Liliia Kuzyeva <gilmirahman@gmail.com>
2019-06-15 19:50:50 +0500

    Выделен класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,6 +11,6 @@ def app(request):
 
 def test_add_contact(app):
     app.open_home_page()
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="Liliia", middlename="Uralovna", lastname="Kuzyeva", nickname="chebi", title="Tester", company="Bank", address="Ekaterinburg, Surikova 60, apt 12", mobile="+79505471137", email="gilmirahman@gmail.com", address2="Ekaterinburg, Selkorovskaya, 2 apt 56"))
-    app.logout()
+    app.session.logout()
