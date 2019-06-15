c7f407aff490ef34132e00f018507efcb94db324
Liliia Kuzyeva <gilmirahman@gmail.com>
2019-06-15 20:02:24 +0500

    Выделен класс для работы с группами

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 __author__ = 'Liliia'
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
 
 class Application:
 
@@ -8,25 +9,7 @@ class Application:
         self.wd = WebDriver()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
-
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
+        self.group = GroupHelper(self)
 
     def create_contact(self, contact):
         wd = self.wd
@@ -66,10 +49,6 @@ class Application:
         wd.find_element_by_name("address2").send_keys(contact.address2)
         # submit contact creation
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
 
     def open_home_page(self):
         wd = self.wd
