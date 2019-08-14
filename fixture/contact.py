f6753c2983cb104d9501ef1f388dd9b2c5f6966e
Liliia Kuzyeva <gilmirahman@gmail.com>
2019-08-14 21:41:06 +0500

    Оптимизирован ввод данных для контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -5,38 +5,25 @@ class ContactHelper:
     def __init__(self, app):
         self.app = app
 
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
     def fill_contact_form(self, contact):
         wd = self.app.wd
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(contact.address2)
+        self.change_field_value("firstname", contact.firstname)
+        self.change_field_value("middlename", contact.middlename)
+        self.change_field_value("lastname", contact.lastname)
+        self.change_field_value("nickname", contact.nickname)
+        self.change_field_value("title", contact.title)
+        self.change_field_value("company", contact.company)
+        self.change_field_value("address", contact.address)
+        self.change_field_value("mobile", contact.mobile)
+        self.change_field_value("email", contact.email)
+        self.change_field_value("address2", contact.address2)
 
     def create(self, contact):
         wd = self.app.wd
--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -1,8 +1,62 @@
 
 from model.contact import Contact
 
-def test_modify_contact(app):
+def test_modify_firstname(app):
     app.open_home_page()
     app.session.login(username="admin", password="secret")
-    app.contact.modify_first(Contact(firstname="Liliia modified", middlename="Uralovna modified", lastname="Kuzyeva edited", nickname="chebi modified", title="Tester modified", company="Bank modified", address="Ekaterinburg, Surikova 60, apt 12", mobile="+79505471137", email="gilmirahman@gmail.com", address2="Ekaterinburg, Selkorovskaya, 2 apt 56"))
+    app.contact.modify_first(Contact(firstname="Liliia modified"))
+    app.session.logout()
+
+def test_modify_middlename(app):
+    app.open_home_page()
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_first(Contact(middlename="Uralovna modified"))
+    app.session.logout()
+
+def test_modify_lastname(app):
+    app.open_home_page()
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_first(Contact(lastname="Kuzyeva edited"))
+    app.session.logout()
+
+def test_modify_nickname(app):
+    app.open_home_page()
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_first(Contact(nickname="chebi modified"))
+    app.session.logout()
+
+def test_modify_title(app):
+    app.open_home_page()
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_first(Contact(title="Tester modified"))
+    app.session.logout()
+
+def test_modify_company(app):
+    app.open_home_page()
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_first(Contact(company="Bank modified"))
+    app.session.logout()
+
+def test_modify_address(app):
+    app.open_home_page()
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_first(Contact(address="Ekaterinburg, Surikova 60, apt 12"))
+    app.session.logout()
+
+def test_modify_mobile(app):
+    app.open_home_page()
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_first(Contact(mobile="+79505471137"))
+    app.session.logout()
+
+def test_modify_email(app):
+    app.open_home_page()
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_first(Contact(email="gilmirahman@gmail.com"))
+    app.session.logout()
+
+def test_modify_address2(app):
+    app.open_home_page()
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_first(Contact(address2="Ekaterinburg, Selkorovskaya, 2 apt 56"))
     app.session.logout()
