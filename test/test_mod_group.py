c54ac2379b85a6c4be452ee09468eae95a21a244
Liliia Kuzyeva <gilmirahman@gmail.com>
2019-08-16 15:46:21 +0500

    Для групп добавлена проверка на количество элементов после добавления, модификации и удаления

--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -5,14 +5,23 @@ from model.group import Group
 def test_modify_first_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first(Group(name="edited group"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_modify_first_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first(Group(header="edited header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_modify_first_footer(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first(Group(footer="edited footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
