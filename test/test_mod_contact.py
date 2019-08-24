a878a24385287f8c670a8c04e92815bb83b46542
Liliia Kuzyeva <gilmirahman@gmail.com>
2019-08-24 21:24:26 +0500

    Реалиована загрузка тестовых данных из файла json для групп и контактов

--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -14,11 +14,3 @@ def test_modify_contact(app):
     new_contacts = app.contact.get_contact_list()
     old_contacts[index-2] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-#def test_modify_middlename(app):
-#    if app.contact.count() == 0:
-#        app.contact.create(Contact(firstname="Marina"))
-#    old_contacts = app.contact.get_contact_list()
-#    app.contact.modify_first(Contact(middlename="Uralovna modified"))
-#    new_contacts = app.contact.get_contact_list()
-#    assert len(old_contacts) == len(new_contacts)
--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -15,19 +15,3 @@ def test_modify_group_name(app):
     new_groups = app.group.get_group_list()
     old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-#def test_modify_first_header(app):
-#    if app.group.count() == 0:
-#        app.group.create(Group(name="test"))
-#    old_groups = app.group.get_group_list()
-#    app.group.modify_first(Group(header="edited header"))
-#    new_groups = app.group.get_group_list()
-#    assert len(old_groups) == len(new_groups)
-
-#def test_modify_first_footer(app):
-#    if app.group.count() == 0:
-#        app.group.create(Group(name="test"))
-#    old_groups = app.group.get_group_list()
-#    app.group.modify_first(Group(footer="edited footer"))
-#    new_groups = app.group.get_group_list()
-#    assert len(old_groups) == len(new_groups)
